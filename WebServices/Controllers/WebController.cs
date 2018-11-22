b4b5eb3ac5807cdc9df77e2dcea5e6aae2413f20
trangtran234 <tn-trang.tran@outlook.com.vn>
2018-11-22 18:03:02 +0700

    Web API and Knockout - create

---
+++ WebServices/Controllers/WebController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    public class WebController : ApiController
+    {
+        private ReservationRepository repo = ReservationRepository.Current;
+        public IEnumerable<Reservation> GetAllReservations()
+        {
+            return repo.GetAll();
+        }
+        public Reservation GetReservation(int id)
+        {
+            return repo.Get(id);
+        }
+        [HttpPost]
+        public Reservation CreateReservation(Reservation item)
+        {
+            return repo.Add(item);
+        }
+        [HttpPut]
+        public bool PutReservation(Reservation item)
+        {
+            return repo.Update(item);
+        }
+        public void DeleteReservation(int id)
+        {
+            repo.Remove(id);
+        }
+    }
+}
